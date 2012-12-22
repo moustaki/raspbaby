566c80a141dd7c137889414e4d814397fb96a4b3
Yves Raimond <yves@dbtune.org>
2012-12-22 14:04:41 +0000

    Factoring out location of pi

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+SSH_PI=pi@raspberrypi
+
 echo "Installing dependencies on pi..."
-ssh pi@raspberrypi "sudo apt-get update && sudo apt-get install graphicsmagick-imagemagick-compat darkice icecast2 screen libmp3lame-dev avahi-daemon libasound2-dev && mkdir raspbaby"
-ssh pi@raspberrypi "sudo apt-get build-dep darkice"
+ssh $SSH_PI "sudo apt-get update && sudo apt-get install graphicsmagick-imagemagick-compat darkice icecast2 screen libmp3lame-dev avahi-daemon libasound2-dev && mkdir raspbaby"
+ssh $SSH_PI "sudo apt-get build-dep darkice"
 echo "Copying code to pi..."
-scp -rd upstart/ darkice/ mjpg-streamer/ darkice-conf/ stream-audio.sh stream-video.sh rc.local pi@raspberrypi:raspbaby/ 
+scp -rd upstart/ darkice/ mjpg-streamer/ darkice-conf/ stream-audio.sh stream-video.sh rc.local $SSH_PI:raspbaby/ 
 echo "Compiling mjpg-streamer..."
-ssh pi@raspberrypi "cd raspbaby/mjpg-streamer && make"
+ssh $SSH_PI "cd raspbaby/mjpg-streamer && make"
 echo "Compling darkice..."
-ssh pi@raspberrypi "cd raspbaby/darkice && ./configure --prefix=/usr --sysconfdir=/usr/share/doc/darkice/examples --with-vorbis-prefix=/usr/lib/arm-linux-gnueabihf/ --with-jack-prefix=/usr/lib/arm-linux-gnueabihf/ --with-alsa-prefix=/usr/lib/arm-linux-gnueabihf/ --with-faac-prefix=/usr/lib/arm-linux-gnueabihf/ --with-aacplus-prefix=/usr/lib/arm-linux-gnueabihf/ --with-samplerate-prefix=/usr/lib/arm-linux-gnueabihf/ --with-lame-prefix=/usr/lib/arm-linux-gnueabihf/ CFLAGS='-march=armv6 -mfpu=vfp -mfloat-abi=hard' && make && sudo make install"
+ssh $SSH_PI "cd raspbaby/darkice && ./configure --prefix=/usr --sysconfdir=/usr/share/doc/darkice/examples --with-vorbis-prefix=/usr/lib/arm-linux-gnueabihf/ --with-jack-prefix=/usr/lib/arm-linux-gnueabihf/ --with-alsa-prefix=/usr/lib/arm-linux-gnueabihf/ --with-faac-prefix=/usr/lib/arm-linux-gnueabihf/ --with-aacplus-prefix=/usr/lib/arm-linux-gnueabihf/ --with-samplerate-prefix=/usr/lib/arm-linux-gnueabihf/ --with-lame-prefix=/usr/lib/arm-linux-gnueabihf/ CFLAGS='-march=armv6 -mfpu=vfp -mfloat-abi=hard' && make && sudo make install"
 echo "Setting up icecast2..."
-ssh pi@raspberrypi "cd raspbaby && sudo cp darkice-conf/icecast-default /etc/default/icecast2 && sudo cp darkice-conf/icecast.xml /etc/icecast2/icecast.xml && sudo /etc/init.d/icecast2 restart"
+ssh $SSH_PI "cd raspbaby && sudo cp darkice-conf/icecast-default /etc/default/icecast2 && sudo cp darkice-conf/icecast.xml /etc/icecast2/icecast.xml && sudo /etc/init.d/icecast2 restart"
 echo "Bumping capture volume up..."
-ssh pi@raspberrypi "amixer -c 1 set Mic 10%"
+ssh $SSH_PI "amixer -c 1 set Mic 10%"
 echo "Updating rc.local..."
-ssh pi@raspberrypi "sudo cp raspbaby/rc.local /etc/rc.local"
+ssh $SSH_PI "sudo cp raspbaby/rc.local /etc/rc.local"
 echo "Updating hostname..."
-ssh pi@raspberrypi "sudo sh -c 'echo raspbaby > /etc/hostname'"
+ssh $SSH_PI "sudo sh -c 'echo raspbaby > /etc/hostname'"
 echo "Rebooting pi..."
-ssh pi@raspberrypi "sudo reboot"
+ssh $SSH_PI "sudo reboot"
